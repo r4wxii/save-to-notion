65c7272cd9895ebd53107a97251bb5c67ed467a1
r4wxii <yn041010@gmail.com>
2022-05-05 16:29:38 +0900

    Feedlyのトークンが失効した際に案内ページを表示するように

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 import { serve } from "https://deno.land/std@0.130.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.130.0/http/file_server.ts";
 
-serve(async () => {
+serve(async (request) => {
   const feedlyToken = Deno.env.get("feedly");
-  const collections =
-    await (await fetch("https://cloud.feedly.com/v3/collections", {
+
+  const collectionsResponse = await fetch(
+    "https://cloud.feedly.com/v3/collections",
+    {
       headers: {
         Authorization: `Bearer ${feedlyToken}`,
       },
-    })).json();
+    },
+  );
+
+  if (!collectionsResponse.ok) {
+    switch (collectionsResponse.status) {
+      case 401:
+        const req = new Request(`${request.url}/expired-feedly-token.html`);
+        return serveDir(req, {
+          fsRoot: "static",
+          urlRoot: "",
+        });
+      default:
+        return new Response("error");
+    }
+  }
+
+  const collections = await collectionsResponse.json();
 
   const results = await Promise.all(
     collections[0].feeds.map(async (feed) =>
